a45956463d766231b56b90f8fb9f7903b60760b1
Thomas Fuller <thomas@mcfuller.com>
2016-05-25 11:06:53 -0700

    Rename MultiBoutStats to multi_bout_stats

--- source/multi_bout_stats/wsgi.py
+++ source/multi_bout_stats/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MultiBoutStats project.
+WSGI config for multi_bout_stats project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MultiBoutStats.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multi_bout_stats.settings")
 
 application = get_wsgi_application()
