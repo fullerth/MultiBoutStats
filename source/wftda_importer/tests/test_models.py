d5bb8317b7232f470a063e0c7738201351215df2
Thomas Fuller <thomas@mcfuller.com>
2016-05-28 14:31:07 -0700

    Remove assert 0=1 from models test

--- source/wftda_importer/tests/test_models.py
+++ source/wftda_importer/tests/test_models.py
@@ -5,6 +5,3 @@ from wftda_importer.models import Player
 class WftdaImporterTests(TestCase):
     def setUp(self):
         pass
-
-    def test_empty_failed_test(self):
-        self.assertEqual(0, 1)
