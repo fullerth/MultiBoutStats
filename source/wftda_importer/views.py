3c6761f46127f5499c52f2296a0bcf56a8f8e0dc
Thomas Fuller <thomas@mcfuller.com>
2016-05-28 14:27:57 -0700

    Add initial blank stat_display template

--- source/wftda_importer/views.py
+++ source/wftda_importer/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def view_stat_list(request):
-    return HttpResponse()
+    context = {}
+    return render(request, 'wftda_importer/stat_display.html', context)
     
