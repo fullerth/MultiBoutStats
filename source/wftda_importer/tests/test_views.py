1901b8bd45118891e98cc889ddc96d81d53032e6
Thomas Fuller <thomas@mcfuller.com>
2016-05-29 00:47:56 -0700

    Add static title to stat display page

--- source/wftda_importer/tests/test_views.py
+++ source/wftda_importer/tests/test_views.py
@@ -10,11 +10,11 @@ class StatDisplayPageTest(TestCase):
     def setUp(self):
         pass
 
-    def test_page_exists(self):
+    def test_page_renders_with_correct_template(self):
         '''
+        Ensure the correct template is used to display player statistics
         '''
         c = Client()
         response = c.get(reverse('display_stats'))
-        
         self.assertTemplateUsed(response, 'wftda_importer/stat_display.html')
         
