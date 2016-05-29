2a0f7941f8d0b21de747e3376c557e05292e7dfb
Thomas Fuller <thomas@mcfuller.com>
2016-05-29 15:42:07 -0700

    Add test for name field

--- source/wftda_importer/models.py
+++ source/wftda_importer/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 
 class Player(models.Model):
-    pass
+    name = models.CharField(max_length=50, null=True)
--- source/wftda_importer/tests/test_models.py
+++ source/wftda_importer/tests/test_models.py
@@ -2,6 +2,9 @@ from django.test import TestCase
 
 from wftda_importer.models import Player
 
-class WftdaImporterTests(TestCase):
+class PlayerTests(TestCase):
     def setUp(self):
         pass
+
+    def test_model_can_store_name(self):
+        p = Player(name="Thump R. Dickhoff")
