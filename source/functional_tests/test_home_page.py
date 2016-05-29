942958ab31ffeb8d8b6c5fef6eaf3df79e08d568
Thomas Fuller <thomas@mcfuller.com>
2016-05-29 15:35:21 -0700

    Add functional test for multiple player names

--- source/functional_tests/test_home_page.py
+++ source/functional_tests/test_home_page.py
@@ -1,14 +1,34 @@
 from .base import FunctionalTest
 
+from wftda_importer.models import Player
+
 class HomePageExists(FunctionalTest):
     '''Open up a page and it's got stats for Jill Nye across multiple available
 bouts
     '''
     def test_multi_bout_stats_title(self):
-        '''Make sure that a title is displayed to the user'''
+        '''Make sure that the correct title is displayed'''
         url = [self.server_url,
                '/'
               ]
         self.browser.get(''.join(url))
 
         self.assertIn("Stats for Jill Nye", self.browser.title)
+
+    def test_multi_bout_stats_shows_bouts(self):
+        '''
+        '''
+        expected_name = "Holly Botts"
+        p = Player()
+        p.name = expected_name
+        p.save()
+
+        url = [self.server_url,
+                '/%(id)'.format({"id":p.id})
+              ]
+
+        self.browser.get(''.join(url))
+
+        self.assertIn("Stats for Holly Botts", self.browser.title)
+
+        
