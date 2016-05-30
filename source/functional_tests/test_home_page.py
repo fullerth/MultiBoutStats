ab8f644d881d22126ba9509ed2088b9aca8edbed
Thomas Fuller <thomas@mcfuller.com>
2016-05-29 21:04:55 -0700

    Change docstrings to use double quotes and be on one line

--- source/functional_tests/test_home_page.py
+++ source/functional_tests/test_home_page.py
@@ -3,15 +3,15 @@ from .base import FunctionalTest
 from wftda_importer.models import Player
 
 class HomePageExists(FunctionalTest):
-    '''Open up a page and it's got stats for Jill Nye across multiple available
+    """Open up a page and it's got stats for Jill Nye across multiple available
 bouts
-    '''
+    """
     def setUp(self):
         self.url_prefix = '/display_stats/'
         super().setUp()
 
     def test_multi_bout_stats_title(self):
-        '''Make sure that the correct title is displayed'''
+        """Make sure that the correct title is displayed"""
         url = [self.server_url,
                self.url_prefix,
               ]
@@ -20,8 +20,7 @@ bouts
         self.assertIn("Stats for Jill Nye", self.browser.title)
 
     def test_multi_bout_stats_shows_bouts(self):
-        '''
-        '''
+        """Ensure that the correct name shows up in the stat detail page"""
         expected_name = "Holly Botts"
         p = Player()
         p.name = expected_name
