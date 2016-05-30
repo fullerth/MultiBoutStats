5d1a445f7fcedf0186e934cee2e87f2a5068f234
Thomas Fuller <thomas@mcfuller.com>
2016-05-29 21:05:46 -0700

    Update docstring to double quotes and one line

--- source/wftda_importer/tests/test_views.py
+++ source/wftda_importer/tests/test_views.py
@@ -8,18 +8,15 @@ from wftda_importer.models import Player
 class StatDisplayPageTest(TestCase):
     def setUp(self):
         pass
-
+        
     def test_page_renders_with_correct_template(self):
-        '''
-        Ensure the correct template is used to display player statistics
-        '''
+        """Ensure the correct template is used to display player statistics"""
         c = Client()
         response = c.get(reverse('display_stats'))
         self.assertTemplateUsed(response, 'wftda_importer/stat_display.html')
-        
+
     def test_stat_page_passes_name_in_context(self):
-        '''
-        '''
+        """Ensure name:Player.name is passed in the detail page context"""
         expected_name = "Luna"
         p = Player()
         p.name = expected_name
--- source/wftda_importer/views.py
+++ source/wftda_importer/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-def view_stat_list(request):
+def view_stat_list(request, player_id=1):
     context = {'name':'Luna'}
     return render(request, 'wftda_importer/stat_display.html', context)
     
