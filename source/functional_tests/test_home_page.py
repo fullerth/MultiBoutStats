904dcd88feef4f7da8670479b6dada211853e5d3
Thomas Fuller <thomas@mcfuller.com>
2016-08-15 10:28:58 -0700

    Fix landing page test to set pk=1

--- source/functional_tests/test_home_page.py
+++ source/functional_tests/test_home_page.py
@@ -6,16 +6,16 @@ class HomePageExists(FunctionalTest):
     """Open up a page and it's got stats for Jill Nye across multiple available
 bouts
     """
+    reset_sequences = True
+
     def setUp(self):
         self.url_prefix = '/display_stats'
         super().setUp()
 
     def test_landing_page_title(self):
-        """Make sure that the correct title is displayed"""
+        """Make sure that the correct title is displayed on the landing page"""
         expected_name = "Jill Nye"
-        p = Player()
-        p.name = expected_name
-        p.save()
+        p = Player.objects.create(pk=1, name=expected_name)
 
         url = [self.server_url,
                self.url_prefix,
@@ -27,18 +27,14 @@ bouts
     def test_detail_page_title(self):
         """Ensure that the correct name shows up in the stat detail page for id=2"""
         expected_name = "Holly Botts"
-        p1 = Player()
-        p1.save()
-        p2 = Player()
-        p2.name = expected_name
-        p2.save()
+        p1 = Player.objects.create()
+        p2 = Player.objects.create(name=expected_name)
 
         url = [self.server_url,
                 '{0}/{1}'.format(self.url_prefix, p2.id)
               ]
 
         self.browser.get(''.join(url))
-
         self.assertIn("Stats for {0}".format(expected_name), self.browser.title)
 
         
