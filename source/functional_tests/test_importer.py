1497e319aa906ad76be4244a6e1a6ed6cdf5cea6
Thomas Fuller <thomas@mcfuller.com>
2016-08-16 09:11:04 -0700

    Add initial input

--- source/functional_tests/test_importer.py
+++ source/functional_tests/test_importer.py
@@ -5,16 +5,28 @@ class ImporterPage(FunctionalTest):
 database"""
 
     def setUp(self):
+        self.url_prefix = '/import'
         super().setUp()
 
     def test_landing_page_title(self):
         """Make sure that the correct title is displayed on the landing page"""
         expected_title = "WFTDA Statbook Importer"
         url = [self.server_url,
-               '/import',
+               self.url_prefix,
               ]
         self.browser.get(''.join(url))
 
         self.assertIn(expected_title, self.browser.title)
+
+    def test_file_input_exists(self):
+        """Make sure that the page contains a file upload input"""
+        url = [self.server_url,
+               self.url_prefix,
+              ]
+        self.browser.get(''.join(url))
+
+        self.browser.find_element_by_id('id_file_input')
+               
+
 
 
