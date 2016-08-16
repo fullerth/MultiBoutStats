649a009d9d1d22c44da76ea711d83e517c5c861a
Thomas Fuller <thomas@mcfuller.com>
2016-08-16 14:13:18 -0700

    Add form to import statbook to wftda_importer

--- source/wftda_importer/views.py
+++ source/wftda_importer/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+
+from django import forms
+
+class WftdaImporterForm(forms.Form):
+    f = forms.FileField()
 
 def view_wftda_importer(request):
-    return(render(request, 'wftda_importer/import.html'))
+    if request.method == 'POST':
+        form = WftdaImporterForm(request.POST, request.FILES)
+        if form.is_valid():
+            return HttpResponseRedirect('/display_stats')
+    else:
+        form = WftdaImporterForm()
+
+    context = {'form':form}
+    return(render(request, 'wftda_importer/import.html', context))
+
