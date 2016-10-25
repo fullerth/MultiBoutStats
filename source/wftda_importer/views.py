dc8f4b1b9c1c81d93014d8b494c05fa2bc35e05e
Thomas Fuller <thomas@mcfuller.com>
2016-10-25 00:19:52 -0700

    Add login_required to import view

--- source/wftda_importer/views.py
+++ source/wftda_importer/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def view_wftda_importer(request):
     return(render(request, 'wftda_importer/import.html'))
